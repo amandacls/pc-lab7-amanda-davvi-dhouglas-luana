3ca214e227726b604dba9d58f92def8e3bde2a69
dhouglasbn <dhouglasbn@gmail.com>
2024-10-01 15:26:00 -0300

    feature: Defining Pedido and Item

--- Pedido.java
+++ Pedido.java
@@ -15,3 +15,64 @@
  * Temos três estados de pedidos:
  * 1. Processado | 2. Rejeitado | 3. Em espera
  */
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Pedido {
+    private final int id;
+
+    private String state;
+
+    private ArrayList<Item> itens;
+
+    public Pedido() {
+        this.state = "em-espera";
+        this.id = new Random().nextInt(999999);
+        this.itens = new ArrayList<Item>();
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void nextState(boolean accepted) {
+        if(this.state.equals("em-espera") && accepted) {
+            this.state = "processado";
+        }
+        if(accepted && this.state.equals("em-espera")) {
+            this.state = "rejeitado";
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<Item> getItens() {
+        return itens;
+    }
+
+    public void setItens(ArrayList<Item> itens) {
+        this.itens = itens;
+    }
+
+    public void addItem(Item item) {
+        this.itens.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.itens.remove(item);
+    }
+
+    public int count(Item item) {
+        int count = 0;
+        for (Item i: this.itens) {
+            if (item.equals(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
