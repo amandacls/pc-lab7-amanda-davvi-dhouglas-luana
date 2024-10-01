61e6c9d16633dfbb2d07a03ee4a8ec341af9b287
119210220 AMANDA CAROLINA LUCIA SANTANA <amanda.santana@lcc3-058.lcc.ufcg.edu.br>
2024-10-01 14:57:16 -0300

    coloca a descrição base nos documentos

--- Pedido.java
+++ Pedido.java
@@ -1,3 +1,17 @@
-/*
+/* BlockingQueue<>
+ * - devem ser colocados em uma fila concorrente, 
+ * onde as threads irão retirar os pedidos para processá-los.
+ * - a fila deve suportar um número limitado de pedidos. 
+ * Se a fila estiver cheia, novos pedidos devem ser bloqueados 
+ * até que haja espaço.
+ * - cada pedido deve conter uma lista de produtos e quantidades.
+ * O sistema verifica a disponibilidade de todos os itens solicitados 
+ * no estoque.
+ * - Se os itens estiverem disponíveis serão descontados do estoque 
+ * e o pedido concluído
+ * - Se os itens não estiverem disponíveis o pedido deve ser rejeitado ou 
+ * ficar pendente, indo para a lista de espera para abastecimento do estoque (Estoque.java)
  * 
+ * Temos três estados de pedidos:
+ * 1. Processado | 2. Rejeitado | 3. Em espera
  */
--- Vendas.java
+++ Vendas.java
@@ -1,3 +1,6 @@
 /*
- * 
+ * Relatório de vendas contém:
+ * - Número total de pedidos processados (variável)
+ * - Valor total das vendas (itera a lista de pedidos processados)
+ * - Número de pedidos rejeitados (por falta de estoque - vem de Pedido.java).
  */
