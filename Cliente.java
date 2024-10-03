261c08f00398bfec6339953be420138881482139
121210718 LUANA DOS SANTOS LIBERATO <luana.liberato@lcc3-058.lcc.ufcg.edu.br>
2024-10-03 16:27:26 -0300

    Adicionando o começo para criar cliente

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,16 @@
+import java.util.concurrent.ScheduledExecutorService;
+
 public class Cliente {
+    private String nome;
+    private ScheduledExecutorService pedidos;
 
+    public Cliente(String nome, ScheduledExecutorService pedidos) {
+        this.nome = nome;
+        this.pedidos = pedidos;
+    }
 
+    public void criarPedido(Item[] itens) {
+        Pedido pedido = new Pedido(itens, this);
+        pedidos.
+    }
 }
